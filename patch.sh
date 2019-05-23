451f853cce1443a5d352d32668e6b8d906d65a31
tildearrow <tildearrow@users.noreply.github.com>
2019-05-23 14:37:00 -0500

    fix patch generator

--- patch.sh
+++ patch.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 klpath=${PWD##*/}
+actualVer=5.15.5
 version=$(sed -rn "s/^set\(PROJECT_VERSION \"([0-9.]+)\"\)$/\1/p" CMakeLists.txt)
 
 cd ..
-if [ ! -e kwin-$version.tar.xz ]
-  then wget https://download.kde.org/stable/plasma/$version/kwin-$version.tar.xz || exit 1
+if [ ! -e kwin-$actualVer.tar.xz ]
+  then wget https://download.kde.org/stable/plasma/$actualVer/kwin-$actualVer.tar.xz || exit 1
 fi
 
-tar -xf kwin-$version.tar.xz
-diff -ruNx .git -x build -x po -x CMakeLists.txt kwin-$version $klpath > kwin-lowlatency-$version.patch
+tar -xf kwin-$actualVer.tar.xz
+diff -ruNx .git -x build -x .kdev4 -x po -x CMakeLists.txt kwin-$actualVer $klpath > kwin-lowlatency-$version.patch
