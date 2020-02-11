9f47d0b68e9116631e9407ec097668b90815a3d6
tildearrow <tildearrow@users.noreply.github.com>
2020-02-11 10:12:54 -0500

    slightly update the po maker

--- po.sh
+++ po.sh
@@ -4,6 +4,8 @@
 klpath=${PWD##*/}
 version=$(sed -rn "s/^set\(PROJECT_VERSION \"([0-9.]+)\"\)$/\1/p" CMakeLists.txt)
 
+rm -r po
+
 cd ..
 if [ ! -e kwin-$version.tar.xz ]
   then wget https://download.kde.org/stable/plasma/$version/kwin-$version.tar.xz || exit 1
